7b31e9d2e402eda914a3aee6a62d5fa778fd5411
Ken Erwin <ken@devopslibrary.com>
2020-09-14 17:27:02 -0400

    Added better welcome message

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,10 @@ PS1='\[\033[01;32m\]\u\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\]$(__git_ps1 " (%
 pre-commit install
 export PIP_USER=no
 clear
-echo 'We are now ready to go, welcome!!'
+echo 'Welcome to the project!'
+echo '-----------------------'
+echo 'Commands for getting started:'
+echo '  hello - Our main entrypoint, invokes the hello world python script within hello.py'
+echo '  pytest - Execute automated testing'
+echo '  pre-commit run - Execute all pre-commit hooks (Automatically called when committing!)'
+echo ''
